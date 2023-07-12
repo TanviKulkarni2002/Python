8b3bf152b1bdeadca3f58f774a9fe0b3352ce03e
Tanvi Kulkarni <90848719+TanviKulkarni2002@users.noreply.github.com>
2023-07-12 18:27:28 +0530

    Update Mouse_Operations.py

--- Mouse_Operations.py
+++ Mouse_Operations.py
@@ -24,30 +24,21 @@ def on_scroll(x,y,dx,dy): #Scrolls
 
 
 #GUI Creation
-def GUI(action_list):
+def GUI():
     
     gui=Tk() #The pop up widget window is initialised
-    gui.title("Automation")
-    
-    #Automating creation of buttons
-    max_rows=3
-    cols=m.ceil(len(action_list)/max_rows)
-
-    for i in range (max_rows):
-        for j in range (cols):
-            label1= Button(gui,text=action_list[i*(j+1)]) #Creating Buttons for each element in action_list
-            label1.grid(row=i,column=j)
+    gui.title("Recording Mouse Operations")
 
 
     gui.geometry("1000x1000") #Hardcoded window size dimensions (rows,cols) format
-    gui.resizable(False,True) #Fixes Window Size (Non-resizable) laterally, but can expand vertically
+    gui.resizable(False,False) #Fixes Window Size (Non-resizable)
 
-    '''Mouse_Ops_Button=Button(gui,text="Record Mouse Operations",command=mouseOps)
-    Mouse_Ops_Button.grid(row=0,column=0)'''
+    Mouse_Ops_Button=Button(gui,text="Record Mouse Operations",command=mouseOps)
+    Mouse_Ops_Button.grid(row=0,column=0)
 
         
 
 
     gui.mainloop() #Creates the loop for the gui widget window
 
-GUI(action_list)
+GUI()
