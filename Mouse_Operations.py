b825c82f9acbc1dcbfdad6d5f8c4a2ab1552b49f
Tanvi Kulkarni <90848719+TanviKulkarni2002@users.noreply.github.com>
2023-07-12 18:22:37 +0530

    Add files via upload

---
+++ Mouse_Operations.py
@@ -0,0 +1,65 @@
+from tkinter import * #Import everything from Tkinter library
+import pyautogui as pg
+from collections import OrderedDict
+import os,sys
+import mouse
+from pynput .mouse import Listener
+import math as m
+
+
+action_list=['Record Single Click', 'Record Double Click', 'Type Text at position', 'Record Mouse Action',
+                'Delete [Step-1]', 'Show all steps', 'Quit']
+
+print("*****   Automation Started   *****")
+
+secDelay     = 2 #secs
+actions=[] #list to store histories and actions
+
+#Single Mouse Click + Scroll Recording
+def mouseOps():
+    with Listener (on_click=on_click,on_scroll=on_scroll) as listener:
+        listener.join()
+
+def on_click(x, y, button, pressed): #Single clicks
+    if pressed:
+        print('Mouse clicked at ({0}, {1}) with {2}'.format(x, y, button))
+
+def on_scroll(x,y,dx,dy): #Scrolls
+    print('Mouse scrolled at ({0}, {1}))'.format(x, y))
+
+
+
+#GUI Creation
+def new_GUI(action_list):
+    
+    gui=Tk() #The pop up widget window is initialised
+    gui.title("Automation")
+    
+    #Automating creation of buttons
+    max_rows=3
+    cols=m.ceil(len(action_list)/max_rows)
+    '''for i in range (len(action_list)): 
+        label1= Button(gui,text=action_list[i],padx=50,pady=50) #Creating Buttons for each element in action_list'''
+
+    for i in range (max_rows):
+        for j in range (cols):
+            label1= Button(gui,text=action_list[i*(j+1)]) #Creating Buttons for each element in action_list
+            label1.grid(row=i,column=j)
+
+
+    gui.geometry("1000x1000") #Hardcoded window size dimensions (rows,cols) format
+    gui.resizable(False,True) #Fixes Window Size (Non-resizable) laterally, but can expand vertically
+
+    '''Mouse_Ops_Button=Button(gui,text="Record Mouse Operations",command=mouseOps)
+    Mouse_Ops_Button.grid(row=0,column=0)'''
+
+        
+
+
+    gui.mainloop() #Creates the loop for the gui widget window
+
+new_GUI(action_list)
+
+
+
+
