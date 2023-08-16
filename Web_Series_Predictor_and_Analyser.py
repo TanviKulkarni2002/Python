5c504bdd8e6fca583bd7a85ad71af4be27778cc0
Tanvi Kulkarni <90848719+TanviKulkarni2002@users.noreply.github.com>
2023-08-16 14:42:00 +0530

    Update Web_Series_Predictor_and_Analyser.py

--- Web_Series_Predictor_and_Analyser.py
+++ Web_Series_Predictor_and_Analyser.py
@@ -119,10 +119,6 @@ predicted_values=kmeans.fit_predict(test_df)
 # Calculating the quality of clustering
 from sklearn.metrics import silhouette_score, calinski_harabasz_score
 from sklearn.metrics import accuracy_score
-
-# Calculating the quality of clustering
-from sklearn.metrics import silhouette_score, calinski_harabasz_score
-from sklearn.metrics import accuracy_score
 # Calculate Silhouette Score
 silhouette_avg = silhouette_score(test_df,predicted_values)
 # Calculate Calinski-Harabasz Index
