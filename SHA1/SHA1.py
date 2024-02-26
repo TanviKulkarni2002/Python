094f3fc66c2ed864f731404121c720d8ad38edf4
Tanvi Kulkarni <90848719+TanviKulkarni2002@users.noreply.github.com>
2024-02-26 16:56:57 +0530

    Delete SHA1 directory

--- SHA1/SHA1.py
+++
@@ -1,7 +0,0 @@
-import hashlib
-with open('SHA1_data.txt') as f:
-    lines = f.readlines()
-f = ''.join(lines)
-result = hashlib.sha1(f.encode())
-print("Hashing value: ")
-print(result.hexdigest())
